b74bcc33019ef6e72a6183b205170f18ce775a1a
Victor Hugo B. S. Miranda <victor-f250@hotmail.com>
2024-12-21 18:05:07 -0300

    Flex painel

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { MatSlideToggleModule,_MatSlideToggleRequiredValidatorModule,
+} from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatSlideToggleModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -12,6 +15,7 @@ export class HomeComponent {
 
   nomeDoUsuario!: string;
   saldo!: number;
+  isOculto: boolean = true;
 
 
 }
