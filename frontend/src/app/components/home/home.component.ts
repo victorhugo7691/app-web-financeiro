a957edd92e3f7c7ffc8797d922ee8f6f02888301
Victor Hugo B. S. Miranda <victor-f250@hotmail.com>
2024-12-21 17:11:15 -0300

    Ajuste da tela principal

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  nomeDoUsuario!: string;
+  saldo!: number;
+
 
 }
